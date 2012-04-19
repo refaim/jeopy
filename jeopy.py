658666bb9b5ba260fc996424f7bc96d6ba0218b3
Roman Kharitonov <refaim.vl@gmail.com>
2012-04-19 14:07:24 +1100

    Split program into separate modules, add simple wxPython application

--- jeopy.py
+++ jeopy.py
@@ -1,109 +1,47 @@
-# coding: utf-8
-
 from __future__ import print_function
 
-import json
-import os
-import shutil
-import string
-import StringIO
 import sys
-import urllib2
-import urlparse
-import zipfile
 
-from lxml import etree
+import wxversion
+wxversion.select("2.8")
+import wx
 
-class JeopyError(Exception): pass
+from common import *
+
+class Application(wx.App):
+    def __init__(self, *args, **kwargs):
+        wx.App.__init__(self, *args, **kwargs)
+        self.retcode = 0
 
 
-def getsuite(url):
-    parsed = urlparse.urlparse(url)
-    if parsed.netloc != 'db.chgk.info':
-        raise JeopyError('Incorrect URL')
+class MainWindow(wx.Frame):
+    def __init__(self, *args, **kwargs):
+        wx.Frame.__init__(self, *args, **kwargs)
+        self.Centre()
+        self.CreateMenu()
 
-    if not url.endswith('/'):
-         url += '/'
-    url = urlparse.urljoin(url, 'fb2')
+    def CreateMenu(self):
+        menubar = wx.MenuBar()
 
-    robj = urllib2.urlopen(url)
-    fobj = StringIO.StringIO()
-    shutil.copyfileobj(robj, fobj)
-    robj.close()
+        menu = wx.Menu()
+        m_exit = menu.Append(wx.ID_EXIT, "E&xit")
+        #self.Bind(wx.EVT_MENU, self.OnClose, m_exit)
+        menubar.Append(menu, "&File")
 
-    archive = zipfile.ZipFile(fobj, 'r')
-    if len(archive.namelist()) != 1:
-        raise JeopyError('Wrong file count in the archive')
-    fb2name = archive.namelist()[0]
-    if os.path.splitext(fb2name)[1] != '.fb2':
-        raise JeopyError('No fb2 files found in the archive')
+        menu = wx.Menu()
+        m_about = menu.Append(wx.ID_ABOUT, "&About")
+        #self.Bind(wx.EVT_MENU, self.OnAbout, m_about)
+        menubar.Append(menu, "&Help")
 
-    with archive.open(fb2name, 'rU') as fb2:
+        self.SetMenuBar(menubar)
 
-        def findall(selector, node):
-            xpath = etree.XPath(selector, namespaces={'fb2': 'http://www.gribuser.ru/xml/fictionbook/2.0'})
-            result = xpath(node)
-            if not result:
-                raise JeopyError('XML parse error, "%s" not found at node %s on line %d' %
-                    (selector, node.tag, node.sourceline))
-            return result
+    def DisplayError(message):
+        wx.MessageBox(message, 'Error', wx.OK | wx.ICON_ERROR)
 
-        def find(selector, node):
-            r = findall(selector, node)
-            return r[0] if len(r) else None
-
-        tree = etree.parse(fb2)
-        title = find('//fb2:book-title/text()', tree)
-        body = find('//fb2:body', tree)
-        sections = {}
-
-        # rounds
-        for section in findall('//fb2:section', body):
-            sectitle = find('.//fb2:title/fb2:p/text()', section)
-            sections[sectitle] = {}
-
-            # questions / answers blocks
-            for i, poem in enumerate(findall('//fb2:poem', section)):
-
-                is_questions = (i + 1) % 2 != 0
-
-                if is_questions:
-
-                    delnum = lambda s: s.strip()[3:]
-
-                    questions = []
-                    rows = map(string.strip, findall('.//fb2:v/text()', poem))
-                    blocktitle = rows[0]
-                    for row in rows[1:]:
-                        if row[0].isdigit():
-                            questions.append(delnum(row))
-                        else:
-                            questions[-1] += u'\n' + row
-                    if len(questions) < 5:
-                        questions.append(delnum(poem.getnext().text))
-
-                else:
-
-                    answers = map(delnum, findall('.//fb2:v/text()', poem))
-                    if len(answers) != 5:
-                        raise JeopyError(
-                            'Not enough answers at line %d' % poem.sourceline)
-
-                    # save
-                    sections[sectitle][blocktitle] = zip(questions, answers)
-
-        suite = { title: sections }
-    return suite
-
-def main():
-    getsuite('http://db.chgk.info/tour/nesp05sv')
-    return 0
 
 if __name__ == '__main__':
-    try:
-        sys.exit(main())
-    except KeyboardInterrupt:
-        print('Interrupted by user')
-    except JeopyError, ex:
-        print(ex.args[0])
-    sys.exit(1)
+    application = Application(redirect=True)
+    window = MainWindow(parent=None, title='JeoPy')
+    window.Show()
+    application.MainLoop()
+    sys.exit(application.retcode)
---
+++ suite.py
@@ -0,0 +1,89 @@
+import json
+import shutil
+import string
+import StringIO
+import urllib2
+import urlparse
+import zipfile
+
+from lxml import etree
+
+from common import *
+
+def getsuite(url):
+    parsed = urlparse.urlparse(url)
+    if parsed.netloc != 'db.chgk.info':
+        raise JeopyError('Incorrect URL')
+
+    if not url.endswith('/'):
+         url += '/'
+    url = urlparse.urljoin(url, 'fb2')
+
+    robj = urllib2.urlopen(url)
+    fobj = StringIO.StringIO()
+    shutil.copyfileobj(robj, fobj)
+    robj.close()
+
+    archive = zipfile.ZipFile(fobj, 'r')
+    if len(archive.namelist()) != 1:
+        raise JeopyError('Wrong file count in the archive')
+    fb2name = archive.namelist()[0]
+    if os.path.splitext(fb2name)[1] != '.fb2':
+        raise JeopyError('No fb2 files found in the archive')
+
+    with archive.open(fb2name, 'rU') as fb2:
+
+        def findall(selector, node):
+            xpath = etree.XPath(selector, namespaces={'fb2': 'http://www.gribuser.ru/xml/fictionbook/2.0'})
+            result = xpath(node)
+            if not result:
+                raise JeopyError('XML parse error, "%s" not found at node %s on line %d' %
+                    (selector, node.tag, node.sourceline))
+            return result
+
+        def find(selector, node):
+            r = findall(selector, node)
+            return r[0] if len(r) else None
+
+        tree = etree.parse(fb2)
+        title = find('//fb2:book-title/text()', tree)
+        body = find('//fb2:body', tree)
+        sections = {}
+
+        # rounds
+        for section in findall('//fb2:section', body):
+            sectitle = find('.//fb2:title/fb2:p/text()', section)
+            sections[sectitle] = {}
+
+            # questions / answers blocks
+            for i, poem in enumerate(findall('//fb2:poem', section)):
+
+                is_questions = (i + 1) % 2 != 0
+
+                if is_questions:
+
+                    delnum = lambda s: s.strip()[3:]
+
+                    questions = []
+                    rows = map(string.strip, findall('.//fb2:v/text()', poem))
+                    blocktitle = rows[0]
+                    for row in rows[1:]:
+                        if row[0].isdigit():
+                            questions.append(delnum(row))
+                        else:
+                            questions[-1] += u'\n' + row
+                    if len(questions) < 5:
+                        questions.append(delnum(poem.getnext().text))
+
+                else:
+
+                    answers = map(delnum, findall('.//fb2:v/text()', poem))
+                    if len(answers) != 5:
+                        raise JeopyError(
+                            'Not enough answers at line %d' % poem.sourceline)
+
+                    # save
+                    sections[sectitle][blocktitle] = zip(questions, answers)
+
+        suite = { title: sections }
+    return suite
